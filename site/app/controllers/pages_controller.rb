34224f700a49c0d9be497e611a557911159cb5d7
Cam <camherringshaw@gmail.com>
2015-02-22 23:14:23 -0500

    Trying to figure out routes
    Trying to add the games page (with games from 494)

--- site/app/controllers/pages_controller.rb
+++ site/app/controllers/pages_controller.rb
@@ -1,7 +1,16 @@
 class PagesController < ApplicationController
 
-def home
-	puts "Hello World!"
-end
+	def home
+		puts "Hello World!"
+	end
+
+	def games
+	end
+
+	def P3Deploy
+	end
+
+	def SplatterGunPrototype
+	end
 
 end
